078919ca5c39562c21b84202b6e2b350e9f431b2
joe1166 <joe1166@qq.com>
2022-03-12 17:52:01 +0800

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.pkucanteen',
+  appName: 'pku食堂',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
